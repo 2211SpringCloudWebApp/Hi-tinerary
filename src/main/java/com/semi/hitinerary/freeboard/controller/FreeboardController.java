f658dfe9e1eb9d43aab9156f578e4d2399425b42
user1 <user1@DESKTOP-MPUKNTA>
2023-03-08 12:32:31 +0900

    기능추가 : 게시판 글쓰기기능 작성중

--- src/main/java/com/semi/hitinerary/freeboard/controller/FreeboardController.java
+++ src/main/java/com/semi/hitinerary/freeboard/controller/FreeboardController.java
@@ -1,5 +1,23 @@
 package com.semi.hitinerary.freeboard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.semi.hitinerary.freeboard.service.FreeboardService;
+
+@Controller
 public class FreeboardController {
+	
+	@Autowired
+	private FreeboardService fService;
+	
+	// 공지사항 등록 화면
+	@RequestMapping(value="/freeboard/writeView.kh", method=RequestMethod.GET)
+	public String writeView() {
+		return "freeboard/write";
+	}
+	
 	
 }
--- src/main/webapp/WEB-INF/views/freeboard/write.jsp
+++ src/main/webapp/WEB-INF/views/freeboard/write.jsp
@@ -7,6 +7,20 @@
 		<title>게시글 작성</title>
 	</head>
 	<body>
-	
+		<h1>자유게시판 새글쓰기</h1>
+		<form>
+    		<select name="fruit">
+      			<option value="free">잡담</option>
+      			<option value="question">질문</option>
+    		</select>
+  		</form>
+		<form action="/notice/write.kh" method="POST" enctype="multipart/form-data">
+			제목 : <input type="text" name="noticeTitle"> <br>
+			작성자 : <input type="text" name="noticeWriter"> <br>
+			내용 : <textarea name="noticeContent"></textarea> <br>
+			첨부파일 : <input type="file" name="uploadFile"> <br>
+			<input type="submit" value="등록">
+			<input type="reset" value="취소">
+		</form>
 	</body>
 </html>

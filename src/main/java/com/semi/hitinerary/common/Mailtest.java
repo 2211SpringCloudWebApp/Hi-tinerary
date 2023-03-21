03cb2b80c5af415d3574b817e21c1d52b77cc39b
user <user@DESKTOP-NKTHM8G>
2023-03-22 02:11:33 +0900

    기능추가 : 메일링 인라인 이미지첨부

--- src/main/java/com/semi/hitinerary/common/Mailtest.java
+++ src/main/java/com/semi/hitinerary/common/Mailtest.java
@@ -1,12 +1,19 @@
 package com.semi.hitinerary.common;
 
+
+import java.io.File;
+
+import javax.activation.FileDataSource;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,12 +24,18 @@ public class Mailtest {
 	private JavaMailSender mailSender;
 	
 	@RequestMapping(value = "/sendMail", method = RequestMethod.GET)
-    public String sendMailTest() throws Exception{
+    public String sendMailTest(HttpServletRequest request) throws Exception{
         
-        String subject = "test 메일";
-        String content = "메일 테스트 내용" + "<img src=\"https://t1.daumcdn.net/cfile/tistory/214DCD42594CC40625\">";
+        String subject = "test 메일입니다";
+        // 메일제목
+        String content = "메일 테스트 내용 <br> 테스트테스트 <img src='cid:logo'/>";
+        // 메일 내용 + src는 아래 이미지 출력
         String from = "truewater98@gmail.com";
+        // 보내는 사람 메일
         String to = "akrsurplus@gmail.com";
+        // 받는사람 메일
+        String filename = "";
+        // 아래 작성
         
         try {
             MimeMessage mail = mailSender.createMimeMessage();
@@ -42,17 +55,27 @@ public class Mailtest {
             mailHelper.setSubject(subject);
             mailHelper.setText(content, true);
             // true는 html을 사용하겠다는 의미입니다.
-            
-            /*
-             * 단순한 텍스트만 사용하신다면 다음의 코드를 사용하셔도 됩니다. mailHelper.setText(content);
-             */
-            
+            String path = request.getSession().getServletContext().getRealPath("resources");
+            // 현재 프로젝트안에 resources에 절대경로 반환
+            filename = path + "\\images\\noThumbnail.png";
+            // resources위치 아래 경로 선택해주기
+            mailHelper.addInline("logo", new FileDataSource(filename));
+            // 경로 선택한 파일 메일 안으로 보내주기            
             mailSender.send(mail);
+            // 메일 발송
             
         } catch(Exception e) {
             e.printStackTrace();
         }
         return "redirect:/";
     }
+	@RequestMapping(value = "/pathtest", method = RequestMethod.GET)
+	public String Pathtest(HttpServletRequest request,Model model) {
+		String path = request.getSession().getServletContext().getRealPath("resources");
+		path += "\\images\\noThumbnail.png";
+        System.out.println("현재 작업 경로: " + path);
+		model.addAttribute("path", path);
+		return "test";
+	}
 
 }
